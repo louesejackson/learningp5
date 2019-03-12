3b174207339eb71ed5043666d23c9dd08a5dad2a
Mary Jackson <mary@marys-mbp-2.home>
2019-03-12 11:44:55 +0000

    make circles

---
+++ p5/make_3_6/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(480,120);
+}
+
+function draw() {
+  background(204);
+  ellipse(278, -100, 400, 400);
+  ellipse(120, 100, 110, 110);
+  ellipse(412, 60, 18, 18);
+  ellipse(20, 20, 10, 10);
+
+}
