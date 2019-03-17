48b51bef9bf0190bc51825486c7355b11aa038cc
Mary Jackson <mary@marys-mbp-2.home>
2019-03-17 13:53:15 +0000

    disable fill and stroke with some issues

---
+++ p5/make_3_15/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(480,120);
+}
+
+function draw() {
+  background(204);  //Black
+  fill(153);  // Medium gray
+  ellipse(132, 82, 200, 200); //gray circle
+  noFill();   //turn off fill
+  ellipse(228, -16, 200, 200); //outline circle
+  noStroke();   //turn off stroke
+  ellipse(268, 118, 200, 200); // doesn't draw
+
+
+}
