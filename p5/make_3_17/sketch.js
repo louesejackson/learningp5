e4e7a73dd61ccceda6510d800cb36071aca5016b
Mary Jackson <mary@marys-mbp-2.home>
2019-03-17 14:18:42 +0000

    transparancy with colour

---
+++ p5/make_3_17/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(480,120);
+  //noStroke();
+}
+
+function draw() {
+  background(204, 226, 225);  //light blue
+  fill(255, 0, 0, 160);   //red colour
+  ellipse(132, 82, 200, 200); //red circle
+  fill(0, 255, 0, 160); //green colour
+  ellipse(228, -16, 200, 200); //  green circle
+  fill(0, 0, 255, 160); //blue colour
+  ellipse(268, 118, 200, 200); //blue circle
+
+
+}
