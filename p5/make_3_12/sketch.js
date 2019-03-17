a5eec5e4c2eec0d2bb51a9ed05dd4d3ec681e154
Mary Jackson <mary@marys-mbp-2.home>
2019-03-17 13:30:56 +0000

    stroke weight, with small error

--- p5/make_3_12/sketch.js
+++ p5/make_3_12/sketch.js
@@ -5,12 +5,13 @@ function setup() {
 
 function draw() {
   background(204);
+  // strokeWeight(20); //Stroke weight to 8 pixels
   ellipse(75,60,90,90);
-  strokeWeight(8); //Stroke weight to 8 pixels
-  ellipse(175, 60, 90,90);
-  ellipse(279, 60, 90, 90);
-  strokeWeight(20); // stroke weight to 20 pixels
-  ellipse(389, 60, 90, 90);
+  // strokeWeight(8); //Stroke weight to 8 pixels
+  // ellipse(175, 60, 90,90);
+  // ellipse(279, 60, 90, 90);
+   strokeWeight(10); // stroke weight to 20 pixels
+// ellipse(389, 60, 90, 90);
 
 
 }
