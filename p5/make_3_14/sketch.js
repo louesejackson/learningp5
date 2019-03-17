183f5d879c7bd051ff93865ed62bb68ab0d2e791
Mary Jackson <mary@marys-mbp-2.home>
2019-03-17 13:36:21 +0000

    paint with grays

---
+++ p5/make_3_14/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(480,120);
+}
+
+function draw() {
+  background(0);  //Black
+  fill(204);      //light gray
+  ellipse(132, 82, 200, 200); // light grey circle
+  fill(153);    //medium gray
+  ellipse(228, -16, 200, 200); // medium gray circle
+  fill(102);  //dark gray
+  ellipse(268, 118, 200, 200);  // dark gray circle
+
+}
