b241afa3d2512b16f45e7d9cbab943212561690b
Mary Jackson <mary@marys-mbp-2.home>
2019-03-17 14:05:40 +0000

    colour circles, with more colour

---
+++ p5/make_3_16/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(480,120);
+  //noStroke();
+}
+
+function draw() {
+  background(0, 26, 51);  //dark blue
+  fill(255, 0, 0); // red colour
+  ellipse(132, 82, 200, 200); //red circle
+  fill(0, 255, 0);  //green colour
+  ellipse(228, -16, 200, 200);  //green circle
+  fill(0, 0, 255); // blue colour
+  ellipse(268, 118, 200, 200); // green circle
+  fill(120, 0, 120); // random purple colour
+  ellipse(350, 100, 200, 200);
+  fill(244, 229, 66); //yellow
+  ellipse(400, 60, 200, 200);
+
+}
