690894dd25eea9247f54c607b843617d436d2a23
Mary Jackson <mary@marys-mbp-2.home>
2019-03-18 10:56:56 +0000

    make an animal pt 1 (fill on ellipse has issues)

---
+++ p5/make_3_20/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(480,120);
+}
+
+function draw() {
+  background(204);
+  //Left creature
+  beginShape();
+  vertex(50, 120);
+  vertex(100, 90);
+  vertex(110, 60);
+  vertex(80, 20);
+  vertex(210, 60);
+  vertex(160, 80);
+  vertex(200, 90);
+  vertex(140, 100);
+  vertex(130, 120);
+  endShape();
+  //fill(0);
+  ellipse(155, 60, 8, 8);
+}
