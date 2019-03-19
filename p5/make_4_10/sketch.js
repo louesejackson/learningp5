79d227445059b7d19a59d0d2c14c491b618db929
Mary Jackson <mary@marys-mbp-2.home>
2019-03-19 11:40:00 +0000

    loops of ellipses to be explained next

---
+++ p5/make_4_10/sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(480, 120);
+  strokeWeight(2);
+}
+
+function draw() {
+  background(0);
+  for (var y = 0; y <= height; y += 40 ) {
+    for (var x = 0; x <= width; x += 40){
+      fill(255, 140);
+      ellipse(x, y, 40, 40);
+      }
+    }
+  }
